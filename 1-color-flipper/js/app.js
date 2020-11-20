9f7607c676a7f4173c7525a8d744c41a5b6b76b5
Mdsomratakbor <somrat876@gmail.com>
2020-11-20 12:00:16 +0600

    Project initialize

---
+++ 1-color-flipper/js/app.js
@@ -0,0 +1,13 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+btn.addEventListener("click", SetColor);
+function SetColor() {
+let randomNumber =  GenerateRandomNumber();
+console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+}
+function GenerateRandomNumber() {
+  return Math.floor(Math.random()*colors.length);
+}
