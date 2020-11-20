54cfa7ffa8ba384bc9a51a1ab0db586a067703df
Mdsomratakbor <somrat876@gmail.com>
2020-11-20 20:46:07 +0600

    counter project is complete

--- 2-counter/js/app.js
+++ 2-counter/js/app.js
@@ -1,21 +1,26 @@
 let count = 0;
 
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll('.btn');
-
-btns.forEach((btn) => {
-    btn.addEventListener('click', function(e){
-        const styles = e.currentTarget.classList;
-        if(styles.contains('decrease'))
-        {
-            count++;
-        }
-        else if(styles.contains('increase')){
-            count--;
-        }
-        else{
-           count =0;
-        }
-    })
-});
-
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+window.onload = function () {
+  btns.forEach((btn) => {
+    btn.addEventListener("click", digitCounter);
+  });
+};
+function digitCounter(e) {
+  const styles = e.currentTarget.classList;
+  if (styles.contains("increase")) {
+    count++;
+  } else if (styles.contains("decrease")) {
+    count--;
+  } else {
+    count = 0;
+  }
+  valueColorSet(count);
+}
+function valueColorSet(count) {
+  if (count > 0) value.style.color = "green";
+  else if (count < 0) value.style.color = "red";
+  else if (count === 0) value.style.color = "#222";
+  value.textContent = count
+}
