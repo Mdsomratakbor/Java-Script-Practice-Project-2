caa37214c256c793bf60110d96387c6ff930401c
Mdsomratakbor <somrat876@gmail.com>
2020-11-21 20:54:24 +0600

    Project 3 reviews is complete

--- 3-reviews/js/app.js
+++ 3-reviews/js/app.js
@@ -1,3 +1,4 @@
+// local reviews data
 const reviews = [
   {
     id: 1,
@@ -10,7 +11,7 @@ const reviews = [
   {
     id: 2,
     author: "Sergey Brin",
-    image: "images/image2.jpg",
+    image: "images/images2.jpg",
     job: "Google Founder",
     text:
       "The company's rapid growth since incorporation has triggered a chain of products, acquisitions, and partnerships beyond Google's core search engine (Google Search). It offers services designed for work and",
@@ -40,12 +41,58 @@ const reviews = [
       "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
 ];
-
+// select items
 const image = document.getElementById("person-img");
 const author = document.getElementById("author");
 const job = document.getElementById("job");
-const text = document.getElementById("info");
+const info = document.getElementById("info");
 
 const prvButton = document.querySelector(".prev-btn");
 const nextButton = document.querySelector(".next-btn");
 const randomButton = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load intial item
+window.addEventListener('DOMContentLoaded',function(){
+ showPerson();
+})
+
+// show person based on item 
+function showPerson(){
+const item = reviews[currentItem];
+image.src = item.image;
+author.textContent = item.author;
+job.textContent = item.job;
+info.textContent = item.text;
+}
+
+// show next person
+nextButton.addEventListener('click', function(){
+  currentItem++;
+  if(currentItem>reviews.length-1){
+   currentItem = 0
+  }
+   showPerson();
+ 
+})
+// show prev person
+prvButton.addEventListener('click', function(){
+   currentItem--;
+   if (currentItem <0) {
+     currentItem = reviews.length -1;
+   }
+   showPerson();
+})
+
+// show random person
+randomButton.addEventListener('click',function(){
+  currentItem = generateRandomNumber();
+  showPerson();
+})
+
+// random number generate
+function generateRandomNumber(){
+  return Math.floor(Math.random()*reviews.length)
+}
