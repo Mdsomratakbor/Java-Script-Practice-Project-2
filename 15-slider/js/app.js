f8e80da5d3ef237d5b67b56ee5f0c06a61e51ec9
Mdsomratakbor <somrat876@gmail.com>
2020-12-04 22:59:58 +0600

    slider project is complete

---
+++ 15-slider/js/app.js
@@ -0,0 +1,45 @@
+const slides = document.querySelectorAll('.slide')
+const nextBtn = document.querySelector('.nextBtn');
+const preBtn = document.querySelector('.prevBtn')
+
+
+slides.forEach((slide, index) => {
+    slide.style.left = `${index*100}%`;
+});
+
+let counter = 0;
+
+nextBtn.addEventListener('click', () => {
+    counter++
+    carousel();
+})
+preBtn.addEventListener('click', () => {
+    counter--;
+    carousel();
+})
+
+function carousel() {
+    // working with slides
+    // if (counter === slides.length) {
+    //     counter = 0;
+    // }
+    // if (counter < 0) {
+    //     counter = slides.length - 1;
+    // }
+    if (counter < slides.length - 1) {
+        nextBtn.style.display = "block"
+    } else {
+        nextBtn.style.display = 'none';
+    }
+    if (counter > 0) {
+        preBtn.style.display = "block"
+    } else {
+        preBtn.style.display = 'none';
+    }
+
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter*100}%)`;
+    })
+}
+
+preBtn.style.display = "none";
